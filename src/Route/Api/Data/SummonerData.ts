6337c03aa338a40ea0bf836aeb075b9ec87fae46
sandro spengler <sandro.spengler@web.de>
2022-03-07 23:02:08 +0100

    Updated controller logic to use local database

--- src/Route/Api/Data/SummonerData.ts
+++ src/Route/Api/Data/SummonerData.ts
@@ -1,16 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
+import { findSummonerByPUUID } from "../../../Repository/SummonerRepository";
+import { formatSummonerForSending } from "../../../Services/FormatDocument";
 import { getSummonerByName } from "../../../Services/Http";
 
 router.get("/:name", async (req, res) => {
   if (req.params.name) {
     try {
-      const summoner = await getSummonerByName(req.params.name);
+      const Response = await getSummonerByName(req.params.name);
 
-      res.status(200).json({
+      // Search by PUUID and by Name to get 1 less requeset
+      // let summonerInDB = await findSummonerByName(Response.data.name);
+      let summonerInDB = await findSummonerByPUUID(Response.data.puuid);
+
+      if (summonerInDB != null) {
+        return res.status(200).json({
+          success: true,
+          result: formatSummonerForSending(summonerInDB),
+        });
+      }
+
+      return res.status(404).json({
         success: true,
-        result: summoner.data,
+        result: "Summoner not Found",
       });
     } catch (error) {
       res.status(500);
