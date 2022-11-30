e66639a639a67fb40a015371d770d8a92a4a0eca
sandro.spengler <sandro.spengler@web.de>
2022-12-01 00:49:11 +0100

    Changed Port for Azure

--- src/Config/config.ts
+++ src/Config/config.ts
@@ -2,7 +2,7 @@
  * This Config contains a centeral entry point for environment variables
  */
 const config = {
-  PORT: Number(process.env.PORT) || 5000,
+  PORT: Number(process.env.PORT) || 1337,
   DB_CONNECTION: process.env.DB_CONNECTION || "",
   LOGGLY_TOKEN: process.env.LOGGLY_TOKEN || "",
 };
