63da32d459e29e4982bcd07c4b23d47522420554
Sandro Spengler Macbook <sandro.spengler@web.de>
2022-12-14 02:16:42 +0100

    Performance Updates 1) Changed query for SummonersByRank in DB to Promise.all() 2) Refactored the formatting for the documents 3) Added index for Summoner.lastMatchUpdate

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import {Application, Request, Response} from "express";
+import { Application, Request, Response } from "express";
 import express from "express";
 
 import * as winston from "winston";
@@ -7,10 +7,10 @@ import cors from "cors";
 // import {config} from "./Config/config";
 require("dotenv").config();
 
-import {swaggerSetup} from "./Services/Swagger/swagger";
-import {createWinstonLoggerWithLoggly} from "./Services/Winston/winston";
-import {connectToMongoDB} from "./MongoDB/mongodb";
-import {Scheduler} from "./Services/Schedule/schedule";
+import { swaggerSetup } from "./Services/Swagger/swagger";
+import { createWinstonLoggerWithLoggly } from "./Services/Winston/winston";
+import { connectToMongoDB } from "./MongoDB/mongodb";
+import { Scheduler } from "./Services/Schedule/schedule";
 
 const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
@@ -24,70 +24,71 @@ const matchRefreshController = require("./Route/Api/Refresh/MatchRefresh");
 const schedule: Scheduler = new Scheduler();
 
 const setupApp = async (): Promise<Application> => {
-  const APP: Application = express();
+	const APP: Application = express();
 
-  APP.use(
-    cors({
-      origin: "*",
-    }),
-  );
+	APP.use(
+		cors({
+			origin: "*",
+		}),
+	);
 
-  APP.get("/", (req: Request, res: Response) => {
-    res.send("<h1>Main Page!!</h1>");
-  });
+	APP.get("/", (req: Request, res: Response) => {
+		res.send("<h1>Main Page!!</h1>");
+	});
 
-  APP.use("/api/data/summoner", jsonParser, summonerController);
-  APP.use("/api/data/league", jsonParser, leaugeController);
-  APP.use("/api/data/match", jsonParser, matchController);
-  APP.use("/api/refresh/summoner", jsonParser, summonerRefreshController);
-  APP.use("/api/refresh/match", jsonParser, matchRefreshController);
+	APP.use("/api/data/summoner", jsonParser, summonerController);
+	APP.use("/api/data/league", jsonParser, leaugeController);
+	APP.use("/api/data/match", jsonParser, matchController);
+	APP.use("/api/refresh/summoner", jsonParser, summonerRefreshController);
+	APP.use("/api/refresh/match", jsonParser, matchRefreshController);
 
-  /**
-   * start and setup of the Application
-   */
-  if (process.env.NODE_ENV !== "test") {
-    try {
-      createWinstonLoggerWithLoggly(process.env.LOGGLY_TOKEN);
+	/**
+	 * start and setup of the Application
+	 */
+	if (process.env.NODE_ENV !== "test") {
+		try {
+			createWinstonLoggerWithLoggly(process.env.LOGGLY_TOKEN);
 
-      console.log(`0: Setup Winston logger`);
-    } catch (error) {
-      console.error(`Could not Setup Winston logger`);
-    }
+			console.log(`0: Setup Winston logger`);
+		} catch (error) {
+			console.error(`Could not Setup Winston logger`);
+		}
 
-    try {
-      swaggerSetup(APP, 1337);
+		try {
+			swaggerSetup(APP, 1337);
 
-      winston.log("info", `Swagger has been setup`);
-    } catch (error) {
-      winston.error(`Could not Setup Swagger`);
-    }
+			winston.log("info", `Swagger has been setup`);
+		} catch (error) {
+			winston.error(`Could not Setup Swagger`);
+		}
 
-    APP.listen(1337, () => {
-      console.log(`0: Server is running on PORT: 1337`);
-    });
+		APP.listen(1337, () => {
+			console.log(`0: Server is running on PORT: 1337`);
+		});
 
-    connectToMongoDB(process.env.DB_CONNECTION).then(
-      () => {},
-      () => {
-        winston.error("error", `Could not connect to MongoDB`);
-        console.error("error", `Could not connect to MongoDB`);
-      },
-    );
-  }
-  if (process.env.RUN_JOB === "stop") {
-    console.log("0: Not running any background jobs");
-    winston.log("info", `Not running any background jobs`);
-  }
+		connectToMongoDB(process.env.DB_CONNECTION).then(
+			() => {},
+			() => {
+				winston.error("error", `Could not connect to MongoDB`);
+				console.error("error", `Could not connect to MongoDB`);
+			},
+		);
+	}
+	if (process.env.RUN_JOB === "stop") {
+		console.log("0: Not running any background jobs");
+		winston.log("info", `Not running any background jobs`);
+	}
 
-  if (process.env.NODE_ENV !== "test" && process.env.RUN_JOB === "start") {
-    console.log("starting");
-    winston.log("info", `starting`);
-    schedule.schedule();
-  }
+	if (process.env.NODE_ENV !== "test" && process.env.RUN_JOB === "start") {
+		console.log("starting");
+		winston.log("info", `starting`);
 
-  return APP;
+		schedule.schedule();
+	}
+
+	return APP;
 };
 
 const startedApp = setupApp();
 
-export {startedApp};
+export { startedApp };
